7375685b5d5a777e38368d057061f5c3a402c964
Muhammed <mohammed.ay99@gmail.com>
2019-08-26 22:14:12 +0300

    Updates

---
+++ precache-manifest.6890a00f3fc618f9307ced068d9db8cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef076df2f6affe1c7f28cf61b3b5cd5",
+    "url": "/loginpagegtu/index.html"
+  },
+  {
+    "revision": "1fb2e161c53b72cfc712",
+    "url": "/loginpagegtu/static/css/main.3d735af9.chunk.css"
+  },
+  {
+    "revision": "d42dd9e2125ea20a657c",
+    "url": "/loginpagegtu/static/js/2.e010c1d5.chunk.js"
+  },
+  {
+    "revision": "1fb2e161c53b72cfc712",
+    "url": "/loginpagegtu/static/js/main.3f522fbf.chunk.js"
+  },
+  {
+    "revision": "069f599538975571d589",
+    "url": "/loginpagegtu/static/js/runtime~main.1eebdc9b.js"
+  }
+]);
